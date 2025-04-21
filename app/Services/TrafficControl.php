18f057d9dd7feb5d382814f400b1cb86916d2f8a
Ahmed Hamouda <ahm3dhamouda@gmail.com>
2025-04-21 22:27:18 +0200

    pass endpoint to traffic control service directly

--- app/Services/TrafficControl.php
+++ app/Services/TrafficControl.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Exceptions\EndpointNotFoundException;
-use App\Modules\Endpoints\Domain\EndpointRepository;
+use App\Modules\Endpoints\Domain\Endpoint;
 use App\Modules\Hits\Domain\HitDto;
 use App\Modules\Hits\Domain\HitRepository;
 use App\Modules\StubStorage\StorageRepository;
@@ -13,19 +12,14 @@ use App\Modules\StubStorage\StorageRepository;
 final readonly class TrafficControl
 {
     public function __construct(
-        private EndpointRepository $endpointRepository,
         private StorageRepository $storageRepository,
         private HitRepository $hitRepository,
     ) {
     }
 
-    public function serve(string $endpointId, string $signature): string
+    public function serve(Endpoint $endpoint, string $signature): string
     {
-        $endpoint = $this->endpointRepository->findById($endpointId);
-
-        if ($endpoint === null) {
-            return throw new EndpointNotFoundException('Endpoint not found');
-        }
+        $endpointId = $endpoint->id();
 
         $this->hitRepository->create(new HitDto($endpointId, $signature));
 
--- tests/Unit/Services/TrafficControlTest.php
+++ tests/Unit/Services/TrafficControlTest.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Services;
 
-use App\Exceptions\EndpointNotFoundException;
 use App\Modules\Endpoints\Domain\Endpoint;
-use App\Modules\Endpoints\Domain\EndpointDto;
-use App\Modules\Endpoints\Domain\EndpointRepository;
 use App\Modules\Hits\Domain\HitDto as EndpointHitDto;
 use App\Modules\Hits\Domain\HitRepository;
 use App\Modules\StubStorage\StorageRepository;
@@ -18,7 +15,6 @@ use Tests\TestCase;
 class TrafficControlTest extends TestCase
 {
     private TrafficControl $service;
-    private EndpointRepository&MockObject $endpointRepository;
     private StorageRepository&MockObject $storageRepository;
     private HitRepository&MockObject $hitRepository;
 
@@ -27,7 +23,6 @@ class TrafficControlTest extends TestCase
         parent::setUp();
 
         $this->service = new TrafficControl(
-            $this->endpointRepository = $this->createMock(EndpointRepository::class),
             $this->storageRepository = $this->createMock(StorageRepository::class),
             $this->hitRepository = $this->createMock(HitRepository::class),
         );
@@ -35,18 +30,14 @@ class TrafficControlTest extends TestCase
 
     public function testServesRequest(): void
     {
-        $id = 'foo';
-        $signature = 'bar';
+        $signature = 'foo';
         $endpointMock = $this->createConfiguredMock(Endpoint::class, [
-            'path' => $path = 'bar',
+            'id' => $id = 'bar',
+            'path' => $path = 'baz',
         ]);
         $endpointHitDto = new EndpointHitDto($id, $signature);
-        $expected = 'baz';
+        $expected = 'foobarbaz';
 
-        $this->endpointRepository->expects(static::once())
-            ->method('findById')
-            ->with(static::identicalTo($id))
-            ->willReturn($endpointMock);
         $this->storageRepository->expects(static::once())
             ->method('fetchById')
             ->with(static::identicalTo($path))
@@ -55,20 +46,8 @@ class TrafficControlTest extends TestCase
             ->method('create')
             ->with(static::equalTo($endpointHitDto));
 
-        $actual = $this->service->serve($id, $signature);
+        $actual = $this->service->serve($endpointMock, $signature);
 
         static::assertSame($expected, $actual);
-    }
-
-    public function testThrowsExceptionInCaseOfNonExistingEndpoint(): void
-    {
-        $this->endpointRepository->expects(static::once())
-            ->method('findById')
-            ->willReturn(null);
-
-        static::expectException(EndpointNotFoundException::class);
-        static::expectExceptionMessage('Endpoint not found');
-
-        $this->service->serve('foo', 'bar');
     }
 }
