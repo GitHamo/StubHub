ee53ae08b9d5ac8c465c1345829c8f966c37a4e0
Ahmed Hamouda <ahm3dhamouda@gmail.com>
2025-04-23 16:42:48 +0200

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // helps identify n+1 query issues during development
         Model::preventLazyLoading(!app()->isProduction());
+
+        if (app()->isProduction() && true === env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
