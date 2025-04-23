56b88964aae649fc52200f4536b8e243e87b75b4
Ahmed Hamouda <ahm3dhamouda@gmail.com>
2025-04-23 19:23:00 +0200

    add some padding on condition for flash messages

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -1,9 +1,17 @@
 @if($errors->any())
-<x-messages.errors :message="Session::get('success')" :errors="$errors" />
+    <div class="p-6">    
+        <x-messages.errors :message="Session::get('success')" :errors="$errors" />
+    </div>
 @endif
+
 @if(Session::has('success'))
-<x-messages.success :message="Session::get('success')" :alertClass="Session::get('alert-class', 'alert-info')" />
+    <div class="p-6">
+        <x-messages.success :message="Session::get('success')" :alertClass="Session::get('alert-class', 'alert-info')" />
+    </div>
 @endif
+
 @if(Session::has('info'))
-<x-messages.info :message="Session::get('info')" :alertClass="Session::get('alert-class', 'alert-info')" />
+    <div class="p-6">
+        <x-messages.info :message="Session::get('info')" :alertClass="Session::get('alert-class', 'alert-info')" />
+    </div>
 @endif
