9e506d38e9fe42441255a790332a714510221dc7
Ahmed Hamouda <ahm3dhamouda@gmail.com>
2025-05-07 11:14:25 +0200

    add download button

--- resources/views/endpoints/partials/listing-item.blade.php
+++ resources/views/endpoints/partials/listing-item.blade.php
@@ -12,6 +12,18 @@
   </td>
   <td class="px-4 py-3 text-right">
     <div class="inline-flex gap-2 justify-end">
+      <!-- Download Button -->
+      <a
+        href="{{ route('endpoints.download', $item->id()) }}"
+        target="_blank"
+        class="inline-flex items-center px-3 py-1 text-xs font-semibold uppercase text-white bg-gradient-to-t from-yellow-700 to-yellow-500 border border-yellow-600 hover:border-yellow-800 rounded"
+      >
+        Download
+        <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M14 3h7v7h-2V6.414l-9.293 9.293-1.414-1.414L17.586 5H14V3z"/>
+          <path d="M5 5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7H5V5z"/>
+        </svg>
+      </a>
       <!-- View Button -->
       <a
         href="{{ route('traffic.serve', $item->id()) }}"
